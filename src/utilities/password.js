7d7d565675c0ddc881a0831228817c12005fc775
Decanini-5 <fernando.decanini@gmail.com>
2022-02-28 10:07:33 -0600

    Pasted all files from original project

---
+++ src/utilities/password.js
@@ -0,0 +1,32 @@
+function generatePasswordFromFile() {
+  const pswd = [];
+  const numberOfCharacters = document.getElementById("pwL").value;
+  const ttError = document.querySelector(".tt-error");
+  let ch1 = Math.floor(Math.random() * (127 - 33)) + 33;
+
+  if (numberOfCharacters < 6) {
+    ttError.style.visibility = "visible";
+    window.setTimeout(() => {
+      ttError.style.visibility = "hidden";
+    }, 3000);
+    document.getElementById("pwL").value = 6;
+    return pswd;
+  }
+  while (
+    [34, 39, 44, 45, 46, 47, 60, 62, 92, 94, 123, 124, 125].includes(ch1)
+  ) {
+    ch1 = Math.floor(Math.random() * (127 - 33)) + 33;
+  }
+  pswd.push(String.fromCharCode(ch1));
+  for (let i = 1; i < numberOfCharacters; i += 1) {
+    let char = Math.floor(Math.random() * (127 - 33)) + 33;
+    while ([34, 39, 44, 47, 60, 62, 92, 94, 123, 124, 125].includes(char)) {
+      char = Math.floor(Math.random() * (127 - 33)) + 33;
+    }
+    pswd.push(String.fromCharCode(char));
+  }
+
+  return pswd.join("");
+}
+
+export default generatePasswordFromFile;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img src="..\safe-password-logo.png" alt="sp_logo" />
+    <div class="text">Enter a length for your password.</div>
+    <div class="input-container">
+      <button
+        class="input-btn minus"
+        onclick="this.parentNode.querySelector('.input').stepDown()"
+      >
+        <div class="symbol-minus">&#129171;</div>
+      </button>
+      <button
+        class="input-btn plus"
+        onclick="this.parentNode.querySelector('.input').stepUp()"
+      >
+        <div class="symbol-plus">&#129169;</div>
+      </button>
+      <input class="input" min="6" value="6" id="pwL" type="number" />
+      <span class="tt-rules"
+        >Your password should be at least 6 characters long.</span
+      >
+      <span class="tt-error">Please enter a length greater than 6.</span>
+    </div>
+    <div class="text">Press button to generate password.</div>
+    <button class="btn" @click="generatePassword">GENERATE PASSWORD</button>
+    <div class="text">My password is:</div>
+    <div class="text" id="pswd">
+      <input class="input-pswd" type="text" id="input-pswd" />
+      <button class="copy" @click="copyToClipboard">
+        <img src="..\copy_icon.png" alt="Copy" />
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import generatePasswordFromFile from "@/utilities/password";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data: () => ({
+    password: null,
+  }),
+  methods: {
+    generatePassword() {
+      this.password = generatePasswordFromFile();
+      document.getElementById("input-pswd").value = this.password;
+    },
+    copyToClipboard() {
+      navigator.clipboard.writeText(this.password);
+    },
   },
 };
 </script>
